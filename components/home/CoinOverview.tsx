2b1a3f088def38f8721628f85141135af4d1d9f0
tesfa27 <tesfamarkos27@gmail.com>
2025-12-31 15:19:53 +0300

    feat: add error handling and fallback components for CoinOverview and TrendingCoins

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -2,22 +2,32 @@ import React from 'react';
 import { fetcher } from '@/lib/coingecko.actions';
 import Image from 'next/image';
 import { formatCurrency } from '@/lib/utils';
+import { CoinOverviewFallback } from './fallback';
 
 const CoinOverview = async () => {
-  const coin = await fetcher<CoinDetailsData>('/coins/bitcoin');
+    let coin;
 
-  return (
-    <div id="coin-overview">
-      <div className="header pt-2">
-        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-        <div className="info">
-          <p>
-            {coin.name} / {coin.symbol.toUpperCase()}
-          </p>
-          <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+    try {
+        coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
+        });
+    } catch (error) {
+        console.error('Error fetching coin overview:', error);
+        return <CoinOverviewFallback />;
+    }
+
+    return (
+        <div id="coin-overview">
+            <div className="header pt-2">
+                <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
+                <div className="info">
+                    <p>
+                        {coin.name} / {coin.symbol.toUpperCase()}
+                    </p>
+                    <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
+
 export default CoinOverview;
